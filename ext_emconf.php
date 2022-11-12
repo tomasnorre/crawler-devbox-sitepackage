f26342ef39b5dd57db80c90e5ae60cb4bed6dbf4
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2022-11-12 14:25:35 +0100

    Add "support" for TYPO3 12 + dev-main

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,9 +9,9 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
-            'fluid_styled_content' => '11.5.0-11.5.99',
-            'rte_ckeditor' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-12.99.99',
+            'fluid_styled_content' => '11.5.0-12.99.99',
+            'rte_ckeditor' => '11.5.0-12.99.99',
         ],
         'conflicts' => [
         ],
@@ -28,5 +28,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Tomas Norre Mikkelsen',
     'author_email' => 'tomasnorre@gmail.com',
     'author_company' => 'TNM',
-    'version' => '1.0.0',
+    'version' => '0.0.1',
 ];
