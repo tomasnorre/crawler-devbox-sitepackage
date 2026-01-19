718d7b794df01ce3f3ecd353ad914eca98f0dc7e
Tomas Norre Mikkelsen <mail@7th-green.com>
2026-01-19 12:26:39 +0100

    feat: update to be TYPO3 14 compatible

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,8 +4,3 @@ defined('TYPO3') or die('Access denied.');
  * Add default RTE configuration
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['crawler_devbox_sitepackage'] = 'EXT:crawler_devbox_sitepackage/Configuration/RTE/Default.yaml';
-
-/***************
- * PageTS
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:crawler_devbox_sitepackage/Configuration/TsConfig/Page/All.tsconfig">');
